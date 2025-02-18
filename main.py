c5dfd8d980fa0048b308f14331acccc5e288321c
Alex-ITs1 <lyvcholeksandr@gmail.com>
2025-02-18 13:55:01 +0100

    Auto-sync Di 18. Feb 13:55:01 CET 2025

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-x = 5
-y = 4
-print(f"Summery is {x + y}, multiplication is {x * y}")
+my_stirng = input("enter your a number:")
+if my_stirng.isdigit():
+    my_integer = int(my_stirng)
+    print(my_integer)
+else:
+    print(f"{my_stirng}is not a number")
